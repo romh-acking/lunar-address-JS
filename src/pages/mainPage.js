4ab77ec1ee8cf9590fa0fcdbf7ddb7f464854302
romh-acking <fake@fake.com>
2021-04-03 13:51:16 -0400

    Changed logo

--- src/pages/mainPage.js
+++ src/pages/mainPage.js
@@ -1,4 +1,4 @@
-import { Button, Grid,  Paper, Typography, makeStyles, Container,} from "@material-ui/core";
+import { Button, Grid, Paper, Typography, makeStyles, Container, } from "@material-ui/core";
 import Brightness4Icon from "@material-ui/icons/Brightness4";
 import Brightness7Icon from "@material-ui/icons/Brightness7";
 import React, { useEffect, useState } from 'react';
@@ -17,6 +17,8 @@ import { romType } from "../lib/romType"
 import { autodetect } from "../lib/autodetect"
 import { decToHex, hexToDec } from "../lib/conversion/HexConversion"
 
+import mainLogo from '../assets/moon.svg';
+
 import { PcToSnes as addressLoROM1PcToSnes, SnesToPc as addressLoROM1SnesToPc } from "../lib/conversion/AddressLoROM1"
 import { PcToSnes as addressLoROM2PcToSnes, SnesToPc as addressLoROM2SnesToPc } from "../lib/conversion/AddressLoROM2"
 import { PcToSnes as addressHiROMPcToSnes, SnesToPc as addressHiROMSnesToPc } from "../lib/conversion/AddressHiROM"
@@ -25,7 +27,7 @@ import { PcToSnes as addressExHiROMPcToSnes, SnesToPc as addressExHiROMSnesToPc 
 import { PcToSnes as addressRAMPcToSnes, SnesToPc as addressRAMSnesToPc } from "../lib/conversion/AddressRAMZSNES"
 import { PcToSnes as addressVRAMPcToSnes, SnesToPc as addressVRAMSnesToPc } from "../lib/conversion/AddressVRAMZSNES"
 
-import { 
+import {
   getLocalLastChangedIsPCAddress, setLocalLastChangedIsPCAddress,
   getLocalLastChangedAddress, setLocalLastChangedAddress,
   getLocalRomTypeDropdown, setLocalRomTypeDropdown
@@ -79,116 +81,116 @@ export default function Users() {
   const [isMounted, setIsmounted] = React.useState(false);
 
   function onSnesAddressChange(value, addressType) {
-      setSnesAddress(value);
+    setSnesAddress(value);
 
-      setLocalLastChangedIsPCAddress(false);
-      setLocalLastChangedAddress(value)
+    setLocalLastChangedIsPCAddress(false);
+    setLocalLastChangedAddress(value)
 
-      setLastChangedIsPCAddress(false);
-      let decimal = hexToDec(value)
+    setLastChangedIsPCAddress(false);
+    let decimal = hexToDec(value)
 
-      let ret;
-      switch(addressType) {
-        case romType.LoROM1:
-          ret = addressLoROM1SnesToPc(decimal);
-          break;
-        case romType.LoROM2:
-          ret = addressLoROM2SnesToPc(decimal);
-          break;
-        case romType.HiROM:
-          ret = addressHiROMSnesToPc(decimal);
-          break;
-        case romType.ExLoROM:
-          ret = addressExLoROMSnesToPc(decimal);
-          break;
-        case romType.ExHiROM:
-          ret = addressExHiROMSnesToPc(decimal);
-          break;
-        case romType.RAM:
-          ret = addressRAMSnesToPc(decimal);
-          break;
-        case romType.VRAM:
-          ret = addressVRAMSnesToPc(decimal);
-          break;
-        default:
-          dispatch(
-            setSnackbar(
-              true,
-              "error",
-              "Invalid rom type:"
-            )
-          )
-          return;
-      }
-
-      setPcAddress(decToHex(ret[0]));
-
-      if (!ret[1]){
+    let ret;
+    switch (addressType) {
+      case romType.LoROM1:
+        ret = addressLoROM1SnesToPc(decimal);
+        break;
+      case romType.LoROM2:
+        ret = addressLoROM2SnesToPc(decimal);
+        break;
+      case romType.HiROM:
+        ret = addressHiROMSnesToPc(decimal);
+        break;
+      case romType.ExLoROM:
+        ret = addressExLoROMSnesToPc(decimal);
+        break;
+      case romType.ExHiROM:
+        ret = addressExHiROMSnesToPc(decimal);
+        break;
+      case romType.RAM:
+        ret = addressRAMSnesToPc(decimal);
+        break;
+      case romType.VRAM:
+        ret = addressVRAMSnesToPc(decimal);
+        break;
+      default:
         dispatch(
           setSnackbar(
             true,
             "error",
-            "Invalid SNES address"
+            "Invalid rom type:"
           )
         )
-      }
+        return;
+    }
+
+    setPcAddress(decToHex(ret[0]));
+
+    if (!ret[1]) {
+      dispatch(
+        setSnackbar(
+          true,
+          "error",
+          "Invalid SNES address"
+        )
+      )
+    }
   }
 
   function onPcAddressChange(value, addressType) {
-      setPcAddress(value);
+    setPcAddress(value);
 
-      setLocalLastChangedIsPCAddress(true);
-      setLocalLastChangedAddress(value)
+    setLocalLastChangedIsPCAddress(true);
+    setLocalLastChangedAddress(value)
 
-      setLastChangedIsPCAddress(true);
-      let decimal = hexToDec(value);
-      let ret;
-      switch(addressType) {
-        case romType.LoROM1:
-          ret = addressLoROM1PcToSnes(decimal);
-          break;
-        case romType.LoROM2:
-          ret = addressLoROM2PcToSnes(decimal);
-          break;
-        case romType.HiROM:
-          ret = addressHiROMPcToSnes(decimal);
-          break;
-        case romType.ExLoROM:
-          ret = addressExLoROMPcToSnes(decimal);
-          break;
-        case romType.ExHiROM:
-          ret = addressExHiROMPcToSnes(decimal);
-          break;
-        case romType.RAM:
-          ret = addressRAMPcToSnes(decimal);
-          break;
-        case romType.VRAM:
-          ret = addressVRAMPcToSnes(decimal);
-          break;
-        default:
-          dispatch(
-            setSnackbar(
-              true,
-              "error",
-              "Invalid rom type"
-            )
-          )
-          return;
-      }
-
-      setSnesAddress(decToHex(ret[0]));
-      
-      if (!ret[1]){
+    setLastChangedIsPCAddress(true);
+    let decimal = hexToDec(value);
+    let ret;
+    switch (addressType) {
+      case romType.LoROM1:
+        ret = addressLoROM1PcToSnes(decimal);
+        break;
+      case romType.LoROM2:
+        ret = addressLoROM2PcToSnes(decimal);
+        break;
+      case romType.HiROM:
+        ret = addressHiROMPcToSnes(decimal);
+        break;
+      case romType.ExLoROM:
+        ret = addressExLoROMPcToSnes(decimal);
+        break;
+      case romType.ExHiROM:
+        ret = addressExHiROMPcToSnes(decimal);
+        break;
+      case romType.RAM:
+        ret = addressRAMPcToSnes(decimal);
+        break;
+      case romType.VRAM:
+        ret = addressVRAMPcToSnes(decimal);
+        break;
+      default:
         dispatch(
           setSnackbar(
             true,
             "error",
-            "Invalid PC address"
+            "Invalid rom type"
           )
         )
-      }
+        return;
+    }
+
+    setSnesAddress(decToHex(ret[0]));
+
+    if (!ret[1]) {
+      dispatch(
+        setSnackbar(
+          true,
+          "error",
+          "Invalid PC address"
+        )
+      )
+    }
   }
-  
+
 
   const handleChange = (event) => {
     setRomTypeDropdown(event.target.value);
@@ -200,13 +202,13 @@ export default function Users() {
 
       if (lastChangedIsPCAddress) {
         onPcAddressChange(pcAddress, romTypeDropdown);
-      } 
+      }
       else {
         onSnesAddressChange(snesAddress, romTypeDropdown);
       }
     }
-  // eslint-disable-next-line
-  }, [ romTypeDropdown ]);
+    // eslint-disable-next-line
+  }, [romTypeDropdown]);
 
   useEffect(() => {
     let lastChangedIsPCAddress_ = getLocalLastChangedIsPCAddress()
@@ -225,11 +227,11 @@ export default function Users() {
 
     setIsmounted(true);
 
-  // eslint-disable-next-line
-  }, [ ]);
+    // eslint-disable-next-line
+  }, []);
 
-	const changeHandlerAutoDetect = (event) => {
-		let file = event.target.files[0];
+  const changeHandlerAutoDetect = (event) => {
+    let file = event.target.files[0];
 
     const reader = new FileReader()
 
@@ -260,45 +262,53 @@ export default function Users() {
     }
 
     reader.readAsArrayBuffer(file);
-	};
+  };
 
   return (
     <>
       <Paper className={classes.paper}>
         <Grid container justify="space-between" alignItems="flex-start">
           <Grid item>
-            <Typography gutterBottom variant="h4">
-              Lunar Address JS
-            </Typography>
+
+
+            <Grid container direction="row" alignItems="center">
+              <Grid item>
+                <img src={mainLogo}></img>
+              </Grid>
+              <Grid item>
+                <Typography variant="h4">&nbsp;&nbsp;Lunar Address JS</Typography>
+              </Grid>
+            </Grid>
+
           </Grid>
 
           <Grid item>
             <Grid container spacing={4} alignItems="center">
               <Grid item>
-              <label htmlFor="upload-photo">
-                <input
-                  style={{ display: 'none' }}
-                  id="upload-photo"
-                  name="upload-photo"
-                  accept=".smc,.sfc"
-                  type="file"
-                  onChange={changeHandlerAutoDetect} 
-                />
+                <label htmlFor="upload-photo">
+                  <input
+                    style={{ display: 'none' }}
+                    id="upload-photo"
+                    name="upload-photo"
+                    accept=".smc,.sfc"
+                    type="file"
+                    onChange={changeHandlerAutoDetect}
+                  />
 
-                <Button color="primary" variant="contained" component="span">
-                  Auto-Detect Type
+                  <Button color="primary" variant="contained" component="span">
+                    Auto-Detect Type
                 </Button>
-              </label>
+                </label>
 
               </Grid>
-              
+
               <Grid item>
                 <AlertDialog />
               </Grid>
-              
+
               <Grid item>
                 <IconButton onClick={_toggleTheme}>
-                { theme === "light" ? <Brightness7Icon /> : <Brightness4Icon /> }
+                  {theme === "light" ? <Brightness7Icon /> : <Brightness4Icon />}
                 </IconButton>
               </Grid>
             </Grid>
@@ -306,36 +316,36 @@ export default function Users() {
         </Grid>
       </Paper>
 
-      <br/>
+      <br />
 
       <Paper className={classes.paper}>
         <>
           <Container>
-             <Grid container spacing={6}>
-               <Grid item md={12}>
-                 <FormControl className={classes.formControl} variant="filled">
-                   <InputLabel>SNES ROM Type</InputLabel>
-                   <Select value={romTypeDropdown} onChange={handleChange}>
-                     {romTypesDropdownItems.map((x) => (
-                         <MenuItem key={x.id} value={x.id}>{x.label}</MenuItem>
-                     ))}
-                   </Select>
-                 </FormControl>
-               </Grid>
-                <Grid item md={6}>
-                 <TextField
-                   label="PC Address"
-                   variant="filled"
-                   value={pcAddress}
-                   onChange={ (e) => onPcAddressChange(e.target.value, romTypeDropdown) }
-                 />
-               </Grid>
-               <Grid item md={6}>
+            <Grid container spacing={6}>
+              <Grid item md={12}>
+                <FormControl className={classes.formControl} variant="filled">
+                  <InputLabel>SNES ROM Type</InputLabel>
+                  <Select value={romTypeDropdown} onChange={handleChange}>
+                    {romTypesDropdownItems.map((x) => (
+                      <MenuItem key={x.id} value={x.id}>{x.label}</MenuItem>
+                    ))}
+                  </Select>
+                </FormControl>
+              </Grid>
+              <Grid item md={6}>
+                <TextField
+                  label="PC Address"
+                  variant="filled"
+                  value={pcAddress}
+                  onChange={(e) => onPcAddressChange(e.target.value, romTypeDropdown)}
+                />
+              </Grid>
+              <Grid item md={6}>
                 <TextField
                   label="SNES Address"
                   variant="filled"
                   value={snesAddress}
-                  onChange={ (e) => onSnesAddressChange(e.target.value, romTypeDropdown) }
+                  onChange={(e) => onSnesAddressChange(e.target.value, romTypeDropdown)}
                 />
               </Grid>
             </Grid>
